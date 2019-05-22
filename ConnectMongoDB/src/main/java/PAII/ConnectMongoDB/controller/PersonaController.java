f75298047f082a082f0ab176853a72241fa23002
Juan Pablo Florez Caicedo <junpaflorez@gmail.com>
2019-05-21 20:07:02 -0500

    buscar por cedula

--- ConnectMongoDB/src/main/java/PAII/ConnectMongoDB/controller/PersonaController.java
+++ ConnectMongoDB/src/main/java/PAII/ConnectMongoDB/controller/PersonaController.java
@@ -4,9 +4,11 @@ import PAII.ConnectMongoDB.entity.Persona;
 import PAII.ConnectMongoDB.service.PersonaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,5 +20,10 @@ public class PersonaController {
     @PostMapping("/guardarPersona")
     public ResponseEntity<?> guardarPersona(@RequestBody Persona persona){
         return ResponseEntity.ok(personaService.guardarPersona(persona));
+    }
+    
+    @GetMapping
+    public ResponseEntity<?> buscarPersona(@RequestParam("cedula") String cedula){
+        return ResponseEntity.ok(personaService.consultarPersona(cedula));
     }
 }
--- ConnectMongoDB/src/main/java/PAII/ConnectMongoDB/service/PersonaService.java
+++ ConnectMongoDB/src/main/java/PAII/ConnectMongoDB/service/PersonaService.java
@@ -2,6 +2,7 @@ package PAII.ConnectMongoDB.service;
 
 import PAII.ConnectMongoDB.entity.Persona;
 import PAII.ConnectMongoDB.repository.PersonaRepository;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,15 @@ public class PersonaService {
     public Persona guardarPersona(Persona persona){
         personaRepository.save(persona);
         return persona;
+    }
+    
+    public Persona consultarPersona(String cedula){
+        Optional<Persona> consulta = Optional.empty();
+        consulta  = personaRepository.findById(cedula); 
+        if(consulta.isPresent()){
+            return consulta.get();
+        }
+        return null;
     }
     
     
