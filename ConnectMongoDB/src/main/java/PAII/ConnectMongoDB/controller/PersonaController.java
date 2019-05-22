ef3d44d473b43e9e7f9d95886d22fd09e06254b4
Juan Pablo Florez Caicedo <junpaflorez@gmail.com>
2019-05-21 19:30:05 -0500

    Update PersonaController.java

--- ConnectMongoDB/src/main/java/PAII/ConnectMongoDB/controller/PersonaController.java
+++ ConnectMongoDB/src/main/java/PAII/ConnectMongoDB/controller/PersonaController.java
@@ -1,10 +1,22 @@
 package PAII.ConnectMongoDB.controller;
 
+import PAII.ConnectMongoDB.entity.Persona;
+import PAII.ConnectMongoDB.service.PersonaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/persona")
 public class PersonaController {
+    @Autowired
+    private PersonaService personaService;
     
+    @PostMapping
+    public ResponseEntity<?> guardarPersona(@RequestBody Persona persona){
+        return ResponseEntity.ok(personaService.guardarPersona(persona));
+    }
 }
